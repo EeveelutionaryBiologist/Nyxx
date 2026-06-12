72fe1ade23fe0e8288bfa3d98c7c9775b72ae9cd
EeeveelutionaryBiologist <christopher@bioinf.uni-leipzig.de>
2026-06-12 10:21:15 +0200

    Loading of base prompt and env parameters from local

--- base_prompt.py
+++ base_prompt.py
@@ -1,2 +1,20 @@
 
-BASE_PROMPT = "You are an agentic AI assistant. Execute tools when local resources are requested."
+import sys
+from pathlib import Path
+
+
+THIS_DIR = Path(__file__).resolve().parent
+BASE_PROMPT_DIR = THIS_DIR / "Personality"
+
+def load_base_prompt() -> str:
+    try:
+        with open(BASE_PROMPT_DIR / "Personality.md") as f:
+            prompt = f.read().strip()
+    except Exception as e:
+        print("[SYSTEM] Critical: Personality.md not found. Was it moved?")
+        sys.exit()
+    
+    return prompt
+
+
+BASE_PROMPT = load_base_prompt()
--- client.py
+++ client.py
@@ -2,29 +2,17 @@
 import os
 import sys
 import json
+from pathlib import Path
 from openai import OpenAI
 
-from base_prompt import BASE_PROMPT
-
-MODEL_NAME = "gemma4:26b"
-BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
-CURRENT_DIR = os.getcwd()
-
-URL_DICT = {
-    "GEMINI": "https://generativelanguage.googleapis.com/v1beta/openai/",
-    "OPENAI": "",
-    "ANTHROPIC": "",
-    "OLLAMA": "http://localhost:11434/v1",
-}
+from base_prompt import THIS_DIR
 
 
-# from pydantic_ai import Agent, RunContext
-
-
+THIS_DIR = Path(__file__).resolve().parent
 
 def load_environment_dict() -> str:
     # TODO: Decoding from encrypt later - right now irrelevant as on local machine
-    with open('env.json') as json_file:
+    with open(THIS_DIR / "env.json") as json_file:
         data = json.load(json_file)
 
     return data
@@ -33,29 +21,29 @@ def load_environment_dict() -> str:
 def initialize_client():
     env = load_environment_dict()
 
-    for key in ["GEMINI_API_KEY", "OPENAI_KEY", "ANTHROPIC_KEY"]:
-        api_key = env.get("GEMINI_API_KEY", "")
-        base_url = URL_DICT.get(key.split("_")[0], "")
+    for provider in ["GEMINI", "OPENAI", "ANTHROPIC"]:
+        api_key = env.get(f"{provider}_API_KEY", "")
 
         if len(api_key) == 0:
             continue
-        else:
-            try:
-                client = OpenAI(
-                    api_key=api_key,
-                    base_url=base_url
-                )
-                return client
-            except Exception as e:
-                print(f"[ERROR] Could not open client: \n{e}")
-                sys.exit()
+
+        base_url = env.get(provider).get("BASE_URL")
+        model = env.get(provider).get("MODEL_NAME")
+
+        try:
+            client = OpenAI(
+                api_key=api_key,
+                base_url=base_url
+            )
+            return client, model
+        except Exception as e:
+            print(f"[ERROR] Could not open client: \n{e}")
+            continue
     
     # Alternatively, use local model as default
     # TODO: Make switching of local models more intuitive
-    base_url = env.get("OLLAMA_URL", "")
-
-    if len(base_url) == 0:
-        base_url = URL_DICT.get("OLLAMA")
+    base_url = env.get("OLLAMA").get("BASE_URL")
+    model = env.get("OLLAMA").get("MODEL_NAME")
 
     try:
         client = OpenAI(
@@ -65,7 +53,7 @@ def initialize_client():
     except Exception as e:
         print(f"[ERROR] Could not open client: \n{e}")
         sys.exit()
-    return client
+    return client, model
 
 
-CLIENT = initialize_client()
+CLIENT, MODEL_NAME = initialize_client()
